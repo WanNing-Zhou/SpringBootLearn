56a0ca4e86a62ad8cde904b2ccb693a542f4ea30
WangNing-Zhou <1879154660@qq.com>
2023-01-05 22:15:01 +0800

    springboot学习

---
+++ boot-05-web-admin/src/main/java/com/zhouzhou/admin/config/MyDatasourceConfig.java
@@ -0,0 +1,16 @@
+package com.zhouzhou.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 周万宁
+ * @className MyDatasourceConfig
+ * @create 2023/1/5-22:12
+ * @description
+ */
+@Deprecated
+public class MyDatasourceConfig {
+
+
+
+}
